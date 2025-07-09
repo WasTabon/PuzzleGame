c9311b46da4a55f50439aacd6643e403723cfbdf
WasTabon <titanium.jen@gmail.com>
2025-07-10 01:45:30 +0300

    blockl

--- Assets/PuzzleGame/Scripts/Block.cs
+++ Assets/PuzzleGame/Scripts/Block.cs
@@ -1,15 +1,43 @@
+using System;
 using UnityEngine;
+
+public enum BlockType
+{
+    Dirt,
+    Rock,
+    Metal
+}
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private BlockType _blockType;
+    
     private Renderer rend;
     private Material originalMat;
     public Material whiteFlashMaterial;
+
+    private int _health;
 
     private void Awake()
     {
         rend = GetComponent<Renderer>();
         originalMat = rend.material;
+    }
+
+    private void Start()
+    {
+        switch (_blockType)
+        {
+            case BlockType.Dirt:
+                _health = 1;
+                break;
+            case BlockType.Rock:
+                _health = 3;
+                break;
+            case BlockType.Metal:
+                _health = 1000;
+                break;
+        }
     }
 
     public void FlashWhite(float duration = 0.05f)
@@ -27,7 +55,7 @@ public class Block : MonoBehaviour
 
     public void Attack()
     {
-        FlashWhite(); // <- вставим сюда, если хочешь миг при любом ударе
-        // Логика разрушения и т.п.
+        FlashWhite();
+        _health--;
     }
 }
